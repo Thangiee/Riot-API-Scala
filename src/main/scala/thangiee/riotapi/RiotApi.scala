610d11652ba7637951ac9ed1d01c5a8cac033d6f
thangiee <Eequalsmc2>
2014-12-15 20:51:01 -0600

    simplify  api code and add team api calls group

--- src/main/scala/thangiee/riotapi/RiotApi.scala
+++ src/main/scala/thangiee/riotapi/RiotApi.scala
@@ -1,63 +1,61 @@
 package thangiee.riotapi
 
 import play.api.libs.json._
-import thangiee.riotapi.summoner.{RunePages, MasteryPages, Summoner}
+import thangiee.riotapi.summoner.{MasteryPages, RunePages, Summoner}
+import thangiee.riotapi.team.Team
 import thangiee.riotapi.utils.ApiCaller
 
 object RiotApi {
   private var reg_ = "na"
-  val summonerVer = "v1.4"
+  val summVer = "v1.4"
+  val teamVer = "v2.4"
 
   def region(region: String) = reg_ = region.toLowerCase
 
   def baseUrl(region: String = reg_) = s"https://$region.api.pvp.net/api/lol/$region"
 
   def summonerByNames(names: List[String], reg: String = reg_)(implicit caller: ApiCaller): Map[String, Summoner] = {
-    implicit val url = s"${baseUrl(reg)}/$summonerVer/summoner/by-name/${names.mkString(",")}?api_key="
-    mapSummoners(names)
+    implicit val url = s"${baseUrl(reg)}/$summVer/summoner/by-name/${names.mkString(",")}?api_key="
+    jsonToMap[String, Summoner](names)
   }
 
   def summonerByIds(ids: List[Long], reg: String = reg_)(implicit caller: ApiCaller): Map[Long, Summoner] = {
-    implicit val url = s"${baseUrl(reg)}/$summonerVer/summoner/${ids.mkString(",")}?api_key="
-    mapSummoners(ids)
+    implicit val url = s"${baseUrl(reg)}/$summVer/summoner/${ids.mkString(",")}?api_key="
+    jsonToMap[Long, Summoner](ids)
   }
 
   def masteryPagesByIds(ids: List[Long], reg: String = reg_)(implicit caller: ApiCaller): Map[Long, MasteryPages] = {
-    val url = s"${baseUrl()}/$summonerVer/summoner/${ids.mkString(",")}/masteries?api_key="
-
-    caller.call(url).map { response =>
-      (for {
-        id ← ids
-        pages ← (Json.parse(response) \ id.toString).asOpt[MasteryPages]
-      } yield id → pages).toMap
-    }.getOrElse(Map())
+    implicit val url = s"${baseUrl(reg)}/$summVer/summoner/${ids.mkString(",")}/masteries?api_key="
+    jsonToMap[Long, MasteryPages](ids)
   }
 
   def summonerNameById(id: Long, reg: String = reg_)(implicit caller: ApiCaller): Option[String] = {
-    val url = s"${baseUrl()}/$summonerVer/summoner/$id/name?api_key="
-
+    val url = s"${baseUrl(reg)}/$summVer/summoner/$id/name?api_key="
     caller.call(url).map { response => (Json.parse(response) \ id.toString).asOpt[String]}.getOrElse(None)
   }
 
   def runePagesByIds(ids: List[Long], reg: String = reg_)(implicit caller: ApiCaller): Map[Long, RunePages] = {
-    val url = s"${baseUrl()}/$summonerVer/summoner/${ids.mkString(",")}/runes?api_key="
-
-    caller.call(url).map { response =>
-      (for {
-        id ← ids
-        pages ← (Json.parse(response) \ id.toString).asOpt[RunePages]
-      } yield id → pages).toMap
-    }.getOrElse(Map())
+    implicit val url = s"${baseUrl(reg)}/$summVer/summoner/${ids.mkString(",")}/runes?api_key="
+    jsonToMap[Long, RunePages](ids)
   }
 
-  private def mapSummoners[A](keys: List[A])(implicit caller: ApiCaller, url: String): Map[A, Summoner] = {
-    caller.call(url).map { response =>
+  def teamBySummonerIds(ids: List[Long], reg: String = reg_)(implicit caller: ApiCaller): Map[Long, List[Team]] = {
+    implicit val url = s"${baseUrl(reg)}/$teamVer/team/by-summoner/${ids.mkString(",")}?api_key="
+    jsonToMap[Long, List[Team]](ids)
+  }
+
+  def teamByTeamIds(ids: List[Long], reg: String = reg_)(implicit caller: ApiCaller): Map[Long, Team] = {
+    implicit val url = s"${baseUrl(reg)}/$teamVer/team/${ids.mkString(",")}?api_key="
+    jsonToMap[Long, Team](ids)
+  }
+
+  private def jsonToMap[A, B: Reads](keys: List[A])(implicit caller: ApiCaller, url: String): Map[A, B] = {
+    caller.call(url).map { jsonResponse =>
       (for {
         key ← keys
-        summ ← (Json.parse(response) \ key.toString).asOpt[Summoner]
-      } yield key → summ).toMap
+        value ← (Json.parse(jsonResponse) \ key.toString).asOpt[B]
+      } yield key → value).toMap
     }.getOrElse(Map())
   }
-
 }
 
