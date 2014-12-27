07d7d95e40388c307a2bdbf5d1ca3e12c5f840dd
thangiee <Eequalsmc2>
2014-12-27 15:56:57 -0600

    make http read time out return a 503 RiotException

--- src/main/scala/thangiee/riotapi/utils/SimpleApiCaller.scala
+++ src/main/scala/thangiee/riotapi/utils/SimpleApiCaller.scala
@@ -2,20 +2,25 @@ package thangiee.riotapi.utils
 
 import thangiee.riotapi.utils.RiotException._
 
+import scala.util.{Failure, Success, Try}
 import scalaj.http.Http
 
 class SimpleApiCaller extends ApiCaller {
-  override def call(url: String)(implicit apiKey: ApiKey): Either[RiotException, String] = {
-    val response = Http(url + apiKey.key).asString
 
-    response.code match {
-      case 200 => Right(response.body)
-      case 400 => Left(RiotException("Bad Request", BadRequest))
-      case 401 => Left(RiotException("Invalid API key", Unauthorized))
-      case 404 => Left(RiotException("Requested data can not be found", DataNotFound))
-      case 429 => Left(RiotException("API key hit limit rate", RateLimit))
-      case 500 => Left(RiotException("Internal server error", ServerError))
-      case 503 => Left(RiotException("Service unavailable", ServiceUnavailable))
+  override def call(url: String)(implicit apiKey: ApiKey): Either[RiotException, String] = {
+    Try(Http(url + apiKey.key).asString) match {
+      case Success(response) =>
+        response.code match {
+          case 200 => Right(response.body)
+          case 400 => Left(RiotException("Bad Request", BadRequest))
+          case 401 => Left(RiotException("Invalid API key", Unauthorized))
+          case 404 => Left(RiotException("Requested data can not be found", DataNotFound))
+          case 429 => Left(RiotException("API key hit limit rate", RateLimit))
+          case 500 => Left(RiotException("Internal server error", ServerError))
+          case 503 => Left(RiotException("Service unavailable", ServiceUnavailable))
+        }
+
+      case Failure(e) => Left(RiotException("Service unavailable", ServiceUnavailable))
     }
   }
 }
