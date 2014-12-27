29247bdf7dcd3fad06af4a766e607cefa7d5b837
thangiee <Eequalsmc2>
2014-12-27 07:41:11 -0600

    add custom exceptions

---
+++ src/main/scala/thangiee/riotapi/utils/RiotException.scala
@@ -0,0 +1,13 @@
+package thangiee.riotapi.utils
+
+case class RiotException(msg: String, errType: RiotException.Value) extends Exception(msg)
+
+object RiotException extends Enumeration {
+  type RiotException = Value
+  val BadRequest         = Value(400)
+  val Unauthorized       = Value(401)
+  val DataNotFound       = Value(404)
+  val RateLimit          = Value(429)
+  val ServerError        = Value(500)
+  val ServiceUnavailable = Value(503)
+}
