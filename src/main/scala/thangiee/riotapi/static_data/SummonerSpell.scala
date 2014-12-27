6de0a9e78f22502e8d4721359a5543c45e31f90e
thangiee <Eequalsmc2>
2014-12-27 07:53:02 -0600

    update api methods signature and refactor helper methods

---
+++ src/main/scala/thangiee/riotapi/static_data/SummonerSpell.scala
@@ -0,0 +1,9 @@
+package thangiee.riotapi.static_data
+
+import play.api.libs.json._
+
+case class SummonerSpell(name: String, description: String, summonerLevel: Int, id: Int, key: String)
+
+object SummonerSpell {
+  implicit val summonerSpellFmt = Json.format[SummonerSpell]
+}
