5de8e79f535143e022a6c4bf47676cb3464ca530
Thangiee <Thangiee0@gmail.com>
2015-08-17 16:09:50 -0500

    Refactor tests

--- src/test/scala/RiotApiSpec.scala
+++ src/test/scala/RiotApiSpec.scala
@@ -10,17 +10,6 @@ class RiotApiSpec extends BaseSpec {
     RiotApi.baseUrl() shouldBe s"https://${RiotApi.regionId}.api.pvp.net/api/lol/${RiotApi.regionId}"
   }
 
-  "Using an invalid api key" should "return a Bad(Unauthorized)" in {
-    RiotApi.key = ""
-
-    RiotApi.summonerById(0).map { summ =>
-      fail("Excepted Unauthorized Error")
-    }.recover {
-      case Unauthorized(_) => // success
-      case _ => fail("Excepted Unauthorized Error")
-    }
-  }
-
   "Parsing json" should "replace missing json field with default value" in new MockScope {
     def json: String = "{\"thangiee\":{\"id\":25011185,\"name\":\"Thangiee\",\"summonerLevel\":30,\"revisionDate\":1435215901000}}"
 
